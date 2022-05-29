d372973ea4910008adfd5df31805c5b360549754
Snoopyyyyy <kentincosnard@gmail.com>
2022-05-29 20:07:44 +0100

    Creation auth system

--- src/Entity/Proprietaire.php
+++ src/Entity/Proprietaire.php
@@ -6,10 +6,12 @@ use App\Repository\ProprietaireRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: ProprietaireRepository::class)]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class Proprietaire implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
